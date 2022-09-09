1aae90993578dff5e3453ef591b655aefb8c3a77
Mohammad Wasim Raza <wasimraza1698@gmail.com>
2022-09-09 22:43:49 +0530

    Added Home page HTML

---
+++ src/app/user/view-tweets/view-tweets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-tweets',
+  templateUrl: './view-tweets.component.html',
+  styleUrls: ['./view-tweets.component.css']
+})
+export class ViewTweetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
