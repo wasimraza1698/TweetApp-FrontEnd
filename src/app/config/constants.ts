be7dedbdc47eaa43918925d57b8e5e27df6bef68
Mohammad Wasim Raza <wasimraza1698@gmail.com>
2022-09-23 10:27:09 +0530

    Added docker file and nginx config

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class Constants {
     public static readonly SITE_TITLE: string = 'TweetApp';
 
-    public static readonly BASE_API_URL: string = 'https://localhost:5001/api/v1.0/tweets/';
+    public static readonly BASE_API_URL: string = 'http://40.81.251.15/api/v1.0/tweets/';
     public static readonly LOGIN_ENDPOINT: string =  'login';
     public static readonly RESET_PASSWORD_ENDPOINT = 'reset-password';
     public static readonly REGISTER_ENDPOINT = 'register';
